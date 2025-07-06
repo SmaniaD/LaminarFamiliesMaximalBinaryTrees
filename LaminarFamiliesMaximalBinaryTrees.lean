18140e09535dd54cc4e3565b7e01577c2ad22b08
Daniel Smania <daniel.smania@gmail.com>
2025-07-06 01:22:47 -0300

    Update LaminarFamiliesMaximalBinaryTrees.lean

--- LaminarFamiliesMaximalBinaryTrees.lean
+++ LaminarFamiliesMaximalBinaryTrees.lean
@@ -1,3 +1,8 @@
+/-!
+Copyright (c) 2025 Daniel Smania.  All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+-/
+
 import Mathlib.MeasureTheory.Measure.MeasureSpace
 import Mathlib.Data.Real.Basic
 import Mathlib.Data.Set.Basic
